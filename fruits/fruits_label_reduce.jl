81e4d607f67d5bac4c3f1d93a4b062396b9c672d
zblanks <zachblanks17@gmail.com>
2018-06-11 10:05:31 -0400

    Updated reduce_label.jl
    Changes
    - Included correction factor
    
    - Added the joinpath() function to the fruits_label_reduce script so
    that it's easier to work with at the cluster.

--- fruits/fruits_label_reduce.jl
+++ fruits/fruits_label_reduce.jl
@@ -1,12 +1,12 @@
-include(ARGS[1] * "reduce_labels_ip.jl")
+include(joinpath(ARGS[1], "reduce_labels_ip.jl"))
 using CSV
 using DataFrames
 
 
 function get_data(args)
     # Get the script arguments for the path
-    r_path = args[2] * "fruits_sim/class_sim.csv"
-    t_path = args[2] * "fruits_sim/comb_sim.csv"
+    r_path = joinpath(args[2], "fruits_sim/class_sim.csv")
+    t_path = joinpath(args[2], "fruits_sim/comb_sim.csv")
     r = CSV.read(r_path, header=false)
     t = CSV.read(t_path, header=false)
     return r, t
@@ -14,11 +14,12 @@ end
 
 
 # Get the similarity data
-function reshape_data(r, t, C::Int, k::Int)
+function reshape_data(r, t, C::Int, L::Int)
     # Re-shape it so that it works for our optimiazation problem
     r = reshape(convert(Array, r[:Column1]), C, 1)
+    r = repeat(r, inner=(1, L))
     t = convert(Array, t[:Column1])
-    t = repeat(t, inner=(1, k))
+    t = repeat(t, inner=(1, L))
 
     # Return the data
     return r, t
@@ -29,21 +30,21 @@ end
 # label mapping for each value
 r, t = get_data(ARGS)
 C = size(r)[1]
-for k = 2:(C-1)
+for L = 2:(C-1)
     # Get our re-shaped version of r and t
-    tmp_r, tmp_t = reshape_data(r, t, C, k)
+    tmp_r, tmp_t = reshape_data(r, t, C, L)
 
     # Run the optimization problem
-    tmp_path = ARGS[2] * "solver_output/ip_output_k" * string(k) * ".txt"
-    tmp_map = label_reduce(C, k, tmp_t, tmp_r, tmp_path)
+    tmp_path = joinpath(ARGS[2], "solver_output/lp_relax_output_L" * string(L) * ".txt")
+    tmp_map = label_reduce(C, L, tmp_t, tmp_r, tmp_path)
     tmp_map = convert(DataFrame, tmp_map)
 
     # Save the map to disk
-    CSV.write(ARGS[2] * "fruit_maps/label_map_" * string(k) * ".csv",
+    CSV.write(joinpath(ARGS[2], "fruit_maps/linear_label_map_" * string(L) * ".csv"),
     tmp_map, header=false)
 
     # Display our progress
-    if k % 5 == 0
-        println("Completed $k problems")
+    if L % 5 == 0
+        println("Completed $L problems")
     end
 end
--- reduce_labels_ip.jl
+++ reduce_labels_ip.jl
@@ -3,63 +3,113 @@ using Gurobi
 using Combinatorics
 
 
-function label_reduce(C::Int, k::Int, t::Array, r::Array, path::String)
+function build_combo_set(C::Int, L::Int)
+    """Defines the combination set needed for our MIP
+
+    Args:
+        C = the original number of classes
+        L = the desired number of new classes
+
+    Returns:
+        (Array): {choose(C-L+1-i, 2) : 0 <= i <= C-L, i ∈ Ζ}
+    """
+
+    # Define the appropriate range of i values
+    i_vals = 0:(C-L-1)
+    combo_set = Vector(length(i_vals))
+    for (i, val) in enumerate(i_vals)
+        combo_set[i] = binomial(C-L+1-val, 2)
+    end
+    return combo_set
+end
+
+
+
+function label_reduce(C::Int, L::Int, t::Array, r::Array, λ::Array, path::String)
     """Generates and runs the label reduction IP
 
     Args:
         C = the original number of classes
-        k = the desired number of new classes
+        L = the desired number of new classes
         t = the similiarity measure between class combinations
         r = the original class similiarity measure
+        λ = correction factor for pairwise approximation
+        path = location to save the solver log file
 
     Returns:
-        (Array): z_{p,l} for all p, l
+        (Array): z_{i,j} for all i, j
     """
 
     # We need to determine the total number of class combinations
     n_comb = binomial(C, 2)
 
+    # We also need to compute the set defining the number of pairwise
+    # combination options
+    combo_set = build_combo_set(C, L)
+
     # Check that the provided arrays have the correct dimension
-    @assert size(t) == (n_comb, k)
-    @assert size(r) == (C, 1)
+    @assert size(t) == (n_comb, L)
+    @assert size(r) == (C, L)
+    @assert size(k) == (length(combo_set), 1)
 
     # Define the model
-    m = Model(solver=GurobiSolver(OutputFlag=0, LogFile=path))
+    m = Model(solver=GurobiSolver(LogFile=path, TimeLimit=5*60))
 
-    # Define our variables
-    @variable(m, z[p=1:C, l=1:k], Bin)
-    @variable(m, x[l=1:k], Bin)
-    @variable(m, δ[p=1:C, l=1:k], Bin)
-    @variable(m, y[p=1:C], Bin)
-    @variable(m, w[s=1:n_comb, l=1:k], Bin)
+    # Define the variables
+    @variable(m, z[i=1:C, j=1:L], Bin)
+    @variable(m, x[j=1:L], Bin)
+    @variable(m, 0 <= y[i=1:C, j=1:L] <= 1)
+    @variable(m, w[s=1:n_comb, j=1:L], Bin)
+    @variable(m, 0 <= γ[s=1:n_comb, j=1:L, k=combo_set] <= 1)
+    @variable(m, θ[j=1:L, k=combo_set], Bin)
+    @variable(m, 0 <= δ[j=1:L, k=combo_set] <= 1)
 
-    # Assign to only class and each label must have at least one class
-    @constraint(m, [p=1:C], sum(z[p, l=1:k]) == 1)
-    @constraint(m, [l=1:k], sum(z[p=1:C, l]) >= 1)
+    # Assignment constraints
+    @constraint(m, [i=1:C], sum(z[i, j=1:L]) == 1)
+    @constraint(m, [j=1:L], sum(z[i=1:C, j]) >= 1)
 
-    # Logic for determining when a label has only one class in it
-    @constraint(m, [l=1:k], sum(z[p=1:C, l]) >= 2 - x[l])
-    M = (C - k + 1) - 2
-    @constraint(m, [l=1:k], sum(z[p=1:C, l]) - 2 <= M*(1-x[l]) - x[l])
-    @constraint(m, [l=1:k, p=1:C], z[p, l] + x[l] >= 2δ[p, l])
-    @constraint(m, [p=1:C], y[p] == sum(δ[p, l=1:k]))
+    # Logic for determining when a column is by itself
+    M = C - L + 1
+    @constraint(m, [j=1:L], sum(z[i=1:C, j]) <= 1 + M*(1-x[j]))
+    @constraint(m, [j=1:L], sum(z[i=1:C, j]) >= 2 - M*x[j])
 
-    # Logical constraints for the siutation where we have more than
-    # one class in a label
+    # Linearize the lone class similiarty variable
+    @constraint(m, [i=1:C, j=1:L], y[i, j] <= z[i, j])
+    @constraint(m, [i=1:C, j=1:L], y[i, j] <= x[j])
+    @constraint(m, [i=1:C, j=1:L], y[i, j] >= z[i, j] + x[j] - 1)
+
+    # Define the auxiliary variables for the presence of combinations
     a = collect(combinations(1:C, 2))
     for (i, elt) in enumerate(a)
         append!(elt, i)
     end
-    for l in 1:k
+    for j in 1:L
         for elt in a
-            @constraint(m, z[elt[1], l] + z[elt[2], l] >= 2w[elt[3], l])
+            @constraint(m, z[elt[1], j] + z[elt[2], j] >= 2 - 2*(1 - w[elt[3], j]))
+            @constraint(m, z[elt[1], j] + z[elt[2], j] <= 1 + 2*w[elt[3], j])
         end
     end
 
-    # Objective
-    @objective(m, Max, vecdot(t, w) + vecdot(r, y))
+    # Linearize the variable for the combination similiarity
+    @constraint(m, [s=1:n_comb, j=1:L, k=combo_set], γ[s, j, k] <= θ[j, k])
+    @constraint(m, [s=1:n_comb, j=1:L, k=combo_set], γ[s, j, k] <= w[s, j])
+    @constraint(m, [s=1:n_comb, j=1:L, k=combo_set], γ[s, j, k] >= θ[j, k] + w[s, j] - 1)
+
+    # Define the constraint which ensures that we are getting the average
+    @constraint(m, [j=1:L], sum(sum(γ[s, j, k]/k for k in combo_set) for s=1:n_comb) <= 1)
+    @constraint(m, [j=1:L], sum(θ[j, k] for k in combo_set) == 1)
+
+    # Add the correction constraints
+    @constraint(m, [j=1:L, k=combo_set], δ[j, k] <= θ[j, k])
+    @constraint(m, [j=1:L, k=combo_set], δ[j, k] <= x[j])
+    @constraint(m, [j=1:L, k=combo_set], δ[j, k] >= θ[j, k] + x[j] - 1)
+
+    # Define the Objective
+    @objective(m, Max, vecdot(r, y) +
+    sum(sum(sum((t[s, j]*γ[s, j, k])/k for k in combo_set) for s=1:n_comb) for j=1:L)) -
+    sum(sum(λ[k]*(θ[j, k] - δ[j, k]) for k in combo_set) for j=1:L)
 
     # Solve the problem
-    solve(m, relaxation=true)
+    solve(m)
     return getvalue(z)
 end
